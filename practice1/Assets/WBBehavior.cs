e4073de5164d4a4b945a2645b6160cd58f12a16c
Gwen <gwendolyn.forde@mail.citytech.cuny.edu>
2019-09-11 11:41:52 -0400

    started wall

---
+++ practice1/Assets/WBBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WBBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject cubePrefab;
+
+    void Start()
+    {
+        //Instantiate(cubePrefab); //makes copy of cube
+    }
+
+    void makeWall()
+    {
+
+    }
+}
---
+++ practice1/Assets/cubeBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float decNumber = 0.01f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveObject(this.transform, Vector3.up * Time.deltaTime);
+    }
+
+    void moveObject(Transform transformToBeMoved, Vector3 amount)
+    {
+        transformToBeMoved.position += amount;
+    }
+}
