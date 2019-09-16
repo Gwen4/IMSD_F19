5692dcd50a85c9c17679d61a305bed2d2a99cedb
Gwen <Gwendolyn.forde@mail.citytech.cuny.edu>
2019-09-16 03:09:39 -0400

    Made wall functions

--- practice1/Assets/WBBehavior.cs
+++ practice1/Assets/WBBehavior.cs
@@ -11,10 +11,43 @@ public class WBBehavior : MonoBehaviour
     void Start()
     {
         //Instantiate(cubePrefab); //makes copy of cube
+        makeWallV();
+        makeWallFloor(0);
+        makeWallFloor(5);
+        makeWallSide(0);
+        makeWallSide(6);
     }
 
-    void makeWall()
+    void makeWallV()
     {
+        for (int col = 0; col <= 5; col++)
+        {
+            for (int row = 0; row <= 5; row++)
+            {
+                Instantiate(cubePrefab, new Vector3(col ,row,0), Quaternion.identity);
+            }
+        }
+    }
 
+    void makeWallFloor(int where)
+    {
+        for (int col = 0; col <= 5; col++)
+        {
+            for (int row = 0; row <= 5; row++)
+            {
+                Instantiate(cubePrefab, new Vector3(col, where, row), Quaternion.identity);
+            }
+        }
+    }
+
+    void makeWallSide(int where)
+    {
+        for (int col = 0; col <= 5; col++)
+        {
+            for (int row = 0; row <= 5; row++)
+            {
+                Instantiate(cubePrefab, new Vector3(where, col, row), Quaternion.identity);
+            }
+        }
     }
 }
