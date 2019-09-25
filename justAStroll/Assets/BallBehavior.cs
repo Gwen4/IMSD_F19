3c667bb89820f65b419b303dadbc32a644d40556
Gwen <Gwendolyn.forde@mail.citytech.cuny.edu>
2019-09-24 21:14:25 -0400

    Starting Project 2
    need to fix camera follow script
    also fix input

---
+++ justAStroll/Assets/BallBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    // Code From Unity Youtube Channel: https://www.youtube.com/watch?v=7C7WWxUxPZE
+    private Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 position = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddTorque(position * speed);
+    }
+}
---
+++ justAStroll/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate() //called frame after update
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, speed*Time.deltaTime);
+        transform.position = smoothedPos;
+    }
+}
