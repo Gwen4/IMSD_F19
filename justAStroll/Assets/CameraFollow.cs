90eadb646f7721da59a41c2c3f45c0d0ab00d412
Gwen <gwendolyn.forde@mail.citytech.cuny.edu>
2019-10-02 11:41:33 -0400

    Created the WallManager
    (change WallMaker to WallManager)
    floor has a material now

--- justAStroll/Assets/CameraFollow.cs
+++ justAStroll/Assets/CameraFollow.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    GameObject Bawl;
-    GameObject Plane;
     public Vector3 offset;
     public float speed;
     void Start()
@@ -17,12 +15,9 @@ public class CameraFollow : MonoBehaviour
     private void LateUpdate() //called frame after update
     {
         Vector3 desiredPos = target.position + offset;
+        transform.LookAt(target);
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, speed*Time.deltaTime);
         transform.position = smoothedPos;
     }
-    private void createWall()
-    {
-        //if bawl approaches edge of plane, spawn a wall
-       // if(Bawl.GetComponent(Transform.)
-    }
+    
 }
---
+++ justAStroll/Assets/WallMakerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMakerBehavior : MonoBehaviour
+{
+
+    //This object needs to track:
+    //Bawls position
+    //if there is/is not a wall already
+    //size of the plane
+    //color of skybox
+
+    GameObject Bawl;
+    GameObject Plane;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
